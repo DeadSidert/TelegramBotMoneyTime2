3c93718b48db1878792b0185d3e45a6cbe0b3abc
Nikita <deadsidert@gmail.com>
2020-11-20 16:12:19 +0300

    Initial commit

---
+++ src/main/java/com/nikita/botter/BotMoneyApplication.java
@@ -0,0 +1,11 @@
+package com.nikita.botter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotMoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotMoneyApplication.class, args);
+    }
+}
