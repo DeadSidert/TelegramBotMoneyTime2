3a36cd8ee3b0797a5f3d941c925da5a04513890b
Nikita <deadsidert@gmail.com>
2020-11-20 19:14:31 +0300

    Добавил затычки на UserService и проверку на существование юзера

--- src/main/java/com/nikita/botter/bot/Bot.java
+++ src/main/java/com/nikita/botter/bot/Bot.java
@@ -2,18 +2,23 @@ package com.nikita.botter.bot;
 
 import com.nikita.botter.model.Channel;
 import com.nikita.botter.model.User;
+import com.nikita.botter.service.UserService;
 import com.nikita.botter.util.TelegramUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+@Slf4j
 public class Bot extends TelegramLongPollingBot {
 
     @Value("${bot.token}")
@@ -23,7 +28,8 @@ public class Bot extends TelegramLongPollingBot {
     private String botName;
     private final ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
     private final HashMap<Integer, User> userHashMap = new HashMap<>();
-    private final List<Channel> channels;
+    private List<Channel> channels;
+    private UserService userService;
 
     @Override
     public String getBotUsername() {
@@ -42,21 +48,51 @@ public class Bot extends TelegramLongPollingBot {
         User user;
         SendMessage sendMessage;
         int userId = 0;
+        String userIdString;
 
         if (isMessageWithText(update)){
             userId = update.getMessage().getFrom().getId();
+            userIdString = String.valueOf(userId);
             command = TelegramUtil.extractCommand(update.getMessage().getText());
 
             // проверка существования юзера
-           if (userHashMap.containsKey(userId)){
+             if (userHashMap.containsKey(userId)){
                user = userHashMap.get(userId);
-           } else {
-
-           }
+             } else if (userService.userExist(userId)){
+               user = userService.findById(userId);
+             }
+             else {
+               user = new User(userId);
+               userHashMap.put(userId, user);
+               userService.update(user);
+             }
 
            if ("/start".equalsIgnoreCase(command)){
-
+              if (!user.isAuth()){
+                  checkUserInChannels(userId);
+              }
+              else {
+                  createUserMenu();
+              }
+              sendMessage = new SendMessage();
+              sendMessage.setChatId(userIdString);
+              sendMessage.setText("Добро пожаловать");
+              sendMessage.setReplyMarkup(keyboardMarkup);
+              log.info("Приветственное сообщение юзеру {}", userIdString);
+              executeWithExceptionCheck(sendMessage);
            }
+
+
+        }
+    }
+
+
+    private void executeWithExceptionCheck(SendMessage sendMessage){
+        try {
+            log.info("Отправили сообщение юзеру {}", sendMessage.getChatId());
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
     }
 
@@ -95,5 +131,6 @@ public class Bot extends TelegramLongPollingBot {
         rowList.add(keyboardRow2);
 
         keyboardMarkup.setKeyboard(rowList);
+        log.info("Создали userMenu");
     }
 }
---
+++ src/main/java/com/nikita/botter/service/UserService.java
@@ -0,0 +1,19 @@
+package com.nikita.botter.service;
+
+
+import com.nikita.botter.model.User;
+
+public class UserService {
+
+    public boolean userExist(int id){
+        return false;
+    }
+
+    public User findById(int id){
+        return null;
+    }
+
+    public User update(User user){
+        return null;
+    }
+}
